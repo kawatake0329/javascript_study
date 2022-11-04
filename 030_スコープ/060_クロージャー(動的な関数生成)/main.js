11da6cf8da987ab5c01a312528ca95c402fe15e9
KawaTake <104962638+kawatake0329@users.noreply.github.com>
2022-11-04 23:08:59 +0900

    クロージャーresultから合計されるやり方が難しい

---
+++ 030_スコープ/060_クロージャー(動的な関数生成)/main.js
@@ -0,0 +1,10 @@
+function addNumberFactory(num) {
+    function addNumber(value){
+        return num + value;
+    }
+    return addNumber
+}
+const add5 = addNumberFactory(5);
+const add10 = addNumberFactory(10);
+const result = add10(10);
+console.log(result);
