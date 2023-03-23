53a4225c9db5c23a145acf40dbdfae106703074a
KawaTake <104962638+kawatake0329@users.noreply.github.com>
2023-03-24 01:10:19 +0900

    javaScriptの基礎 変数、型

---
+++ 超JavaScript/main.js
@@ -0,0 +1,71 @@
+let count = 0;
+count = 30;
+let newCount = 0;
+const daysInweek = 7;
+let tomatoCount;
+let $tomato$Count;
+let _tomato_Count;
+let tomatoCount7;
+let トマトカウント;
+
+let additioinResult = 2 + 5;
+console.log(additioinResult);
+let result = 5;
+result = result + 10;
+result = result + 1;
+result++;
+++result;
+let number = 10;
+number = -4;
+number = 32.42;
+let string = 'Hello';
+number = 'something';
+const userName = 'Yoshipi';
+string = 'Hello ' + userName + '!';
+string = `Hello ${userName}!`;
+string = "'``'\\";
+string = "'";
+string = 'Hello\nYoshipi!';
+string = `Hello
+Yoshipi`;
+const userInput = '10';
+let calcResult;
+calcResult = Number(userInput) + 10;
+console.log(calcResult);
+calcResult = parseInt(userInput) + 10;
+console.log(calcResult);
+calcResult = parseFloat(userInput) + 10;
+calcResult = +userInput + 10;
+const tenNumber = 10;
+calcResult = '10' + String(tenNumber);
+calcResult = '10' + tenNumber.toString();
+console.log(calcResult);
+
+let boolean = true;
+boolean = false;
+
+let array = ['apple', 'banana', 'grape'];
+array[(1, 2, 3)];
+array = [1, 'apple'.toUpperCase, array];
+array = ['apple', 'banana', 'grape'];
+console.log(array[0]);
+array = [];
+array.push('apple');
+// 上記の文で一つずつ配列を入れることができる。
+const coffee = {
+  name: 'Chocolate Mocha',
+  size: 350,
+  isHot: true,
+  toppings: ['Cinnamon', 'Caramel'],
+  nutritions: {
+    calories: 430,
+    sugars: 53,
+    caffeine: 100,
+  },
+};
+console.log(coffee.size);
+coffee.isHot = false;
+coffee.varista = 'Yoshipi';
+console.log(coffee.varista);
+let userInfo = null;
+userInfo = undefined;
